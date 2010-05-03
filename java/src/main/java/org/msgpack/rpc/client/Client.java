ab342e7a016f4f234972b4dab645b98fc2ebf7ed
Kazuki Ohta <kazuki.ohta@gmail.com>
2010-05-04 03:49:25 +0900

    java: hide EventLoop from the user

--- java/src/main/java/org/msgpack/rpc/client/Client.java
+++ java/src/main/java/org/msgpack/rpc/client/Client.java
@@ -1,7 +1,19 @@
 package org.msgpack.rpc.client;
 
 public class Client extends Session {
+	protected EventLoop loop;
+	
+	public Client(String host, int port) {
+		this(host, port, new EventLoop());
+	}
+
 	public Client(String host, int port, EventLoop loop) {
 		super(new Address(host, port), loop);
+		this.loop = loop;
+	}
+	
+	public void close() {
+		super.close();
+		loop.shutdown();
 	}
 }
