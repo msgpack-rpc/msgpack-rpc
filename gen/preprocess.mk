9fc1b1e5a28fdf09cbad76a2158460c0114b983f
frsyuki <frsyuki@users.sourceforge.jp>
2010-05-17 04:45:14 +0900

    gen: update preprocess

--- gen/preprocess.mk
+++ gen/preprocess.mk
@@ -8,27 +8,27 @@ TTURL = http://rubygems.org/downloads/$(TTVER).gem
 $(TTVER).gem:
 	wget $(TTURL) || curl -O $(TTURL) || fetch $(TTURL)
 
-treetop/bin/tt: $(TTVER).gem
-	mkdir -p treetop
-	cd treetop && tar xvf ../$(TTVER).gem
-	cd treetop && tar zxvf data.tar.gz
+$(TTVER)/bin/tt: $(TTVER).gem
+	mkdir -p $(TTVER)
+	cd $(TTVER) && tar xvf ../$(TTVER).gem
+	cd $(TTVER) && tar zxvf data.tar.gz
 	touch $@
 
-rubylib/rubygems: treetop/bin/tt
+rubylib/rubygems: $(TTVER)/bin/tt
 	mkdir -p rubylib
-	cp -rf treetop/lib/* rubylib/
-	cp -rf treetop/README.md rubylib/treetop/
-	cp -rf treetop/LICENSE rubylib/treetop/
+	cp -rf $(TTVER)/lib/* rubylib/
+	cp -rf $(TTVER)/README.md rubylib/treetop/
+	cp -rf $(TTVER)/LICENSE rubylib/treetop/
 	echo "# dummy" > $@
 
 mprpc_idl.tt: mprpc_idl.tt.mpl mplex.rb
 	$(RUBY) -rmplex -e 'Mplex.write("mprpc_idl.tt.mpl", "$@")'
 
-mprpc_idl.rb: treetop/bin/tt mprpc_idl.tt rubylib/rubygems
-	$(RUBY) -Irubylib treetop/bin/tt -o $@ mprpc_idl.tt
+mprpc_idl.rb: $(TTVER)/bin/tt mprpc_idl.tt rubylib/rubygems
+	$(RUBY) -I$(TTVER)/lib $(TTVER)/bin/tt -o $@ mprpc_idl.tt
 
 clean:
-	rm -rf treetop rubylib mprpc_idl.tt mprpc_idl.rb
+	rm -rf $(TTVER) rubylib mprpc_idl.tt mprpc_idl.rb
 
 distclean: clean
 	rm -rf $(TTVER).gem
