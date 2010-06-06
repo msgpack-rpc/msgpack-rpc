0cc9d98a0185584b34ab829433b07b7bff5fb632
UENISHI Kota <kuenishi+github@gmail.com>
2010-06-07 02:05:37 +0900

    erlang: adding automated test

---
+++ erlang/test/sample_SUITE.erl
@@ -0,0 +1,10 @@
+%%%-------------------------------------------------------------------
+%%% File    : sample_SUITE.erl
+%%% Author  : UENISHI Kota <kuenishi@gmail.com>
+%%% Description : test suite for sample application.
+%%%
+%%% Created :  7 Jun 2010 by UENISHI Kota <kuenishi@gmail.com>
+%%%-------------------------------------------------------------------
+-module(sample_SUITE).
+-author('kuenishi@gmail.com').
+
