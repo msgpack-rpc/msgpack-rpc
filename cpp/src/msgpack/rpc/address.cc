8080117fd1c733da0ad69ff4b6e1ed077c238b0e
frsyuki <frsyuki@users.sourceforge.jp>
2010-07-04 07:40:56 +0900

    cpp: rewrite address class

--- cpp/src/msgpack/rpc/address.cc
+++ cpp/src/msgpack/rpc/address.cc
@@ -24,108 +24,43 @@ namespace msgpack {
 namespace rpc {
 
 
-//void address::copy_n46(const address& addr)
-//{
-//	char* tmp;
-//
-//	switch(m_type) {
-//	case IPV4:
-//	case IPV6:
-//		break;
-//
-//	case PATH:
-//		tmp = strdup(addr.m.path.str);
-//		if(tmp == NULL) {
-//			throw std::bad_alloc();
-//		}
-//		m.path.str = tmp;
-//		break;
-//
-//	default:
-//		m.ext.size = addr.m.ext.size;
-//		if(m.ext.size > sizeof(m.ext.buffer.stack)) {
-//			m.ext.buffer.heap = (char*)malloc(m.ext.size);
-//			if(m.ext.buffer.heap == NULL) {
-//				throw std::bad_alloc();
-//			}
-//			memcpy(m.ext.buffer.heap, addr.m.ext.buffer.heap, m.ext.size);
-//		} else {
-//			memcpy(m.ext.buffer.stack, addr.m.ext.buffer.stack, m.ext.size);
-//		}
-//		break;
-//	}
-//}
+void address::copy_ex(const address& a)
+{
+	m.ex.family = a.m.ex.family;
+	m.ex.addrlen = a.m.ex.addrlen;
+	m.ex.addr = (struct sockaddr*)malloc(a.m.ex.addrlen);
+	if(m.ex.addr == NULL) {
+		throw std::bad_alloc();
+	}
+	memcpy(m.ex.addr, a.m.ex.addr, a.m.ex.addrlen);
+}
 
-//void address::copy_op_n46(const address& addr)
-//{
-//	uint16_t heap_size = 0;
-//	char* heap = NULL;
-//
-//	switch(m_type) {
-//	case IPV4:
-//	case IPV6:
-//		break;
-//
-//	case PATH:
-//		heap_size = strlen(m.path.str)+1;
-//		heap = m.path.str;
-//		break;
-//
-//	default:
-//		if(m.ext.size > sizeof(m.ext.buffer.stack)) {
-//			heap_size = m.ext.size;
-//			heap = m.ext.buffer.heap;
-//		}
-//		break;
-//	}
-//
-//	switch(addr.m_type) {
-//	case IPV4:
-//	case IPV6:
-//		break;
-//
-//	case PATH: {
-//			uint16_t size = strlen(addr.m.path.str)+1;
-//			if(size <= heap_size) {
-//				m.path.str = heap;
-//				memcpy(m.path.str, addr.m.path.str, size);
-//			} else {
-//				char* tmp = strdup(addr.m.path.str);
-//				if(tmp == NULL) {
-//					throw std::bad_alloc();
-//				}
-//				m.path.str = tmp;
-//				if(heap != NULL) { free(heap); }
-//			}
-//			break;
-//		}
-//
-//	default:
-//		if(addr.m.ext.size <= sizeof(m.ext.buffer.stack)) {
-//			m.ext.size = addr.m.ext.size;
-//			memcpy(m.ext.buffer.stack, addr.m.ext.buffer.stack, m.ext.size);
-//			if(heap != NULL) { free(heap); }
-//
-//		} else if(addr.m.ext.size <= heap_size) {
-//			m.ext.size = addr.m.ext.size;
-//			m.ext.buffer.heap = heap;
-//			memcpy(m.ext.buffer.heap, addr.m.ext.buffer.heap, m.ext.size);
-//
-//		} else {
-//			char* tmp = (char*)malloc(addr.m.ext.size);
-//			if(tmp == NULL) {
-//				throw std::bad_alloc();
-//			}
-//			m.ext.size = addr.m.ext.size;
-//			m.ext.buffer.heap = tmp;
-//			memcpy(m.ext.buffer.heap, addr.m.ext.buffer.heap, m.ext.size);
-//			if(heap != NULL) { free(heap); }
-//		}
-//	}
-//}
+void address::assign_ex(const address& o)
+{
+	if(is_ex() && m.ex.addrlen >= o.m.ex.addrlen) {
+		m.ex.family = o.m.ex.family;
+		m.ex.addrlen = o.m.ex.addrlen;
+		memcpy(m.ex.addr, o.m.ex.addr, o.m.ex.addrlen);
+		return;
+	}
+
+	struct sockaddr* addr = (struct sockaddr*)malloc(o.m.ex.addrlen);
+	if(addr == NULL) {
+		throw std::bad_alloc();
+	}
+
+	if(is_ex()) {
+		free(m.ex.addr);
+	}
+	memcpy(addr, o.m.ex.addr, o.m.ex.addrlen);
+	m.ex.family = o.m.ex.family;
+	m.ex.addrlen = o.m.ex.addrlen;
+	m.ex.addr = addr;
+}
 
 
-void ip_address::resolve(const char* host, uint16_t port, int v6) {
+void ip_address::resolve(const char* host, uint16_t port, int v6)
+{
 	addrinfo hints = {};
 	if(v6 == 0) {
 		hints.ai_family = AF_INET;
@@ -147,12 +82,8 @@ void ip_address::resolve(const char* host, uint16_t port, int v6) {
 			if(rp->ai_family != AF_INET || rp->ai_addrlen < sizeof(struct sockaddr_in)) {
 				continue;
 			}
-			struct sockaddr_in* addr = (struct sockaddr_in*)rp->ai_addr;
-
-			m_type = IPV4;
+			memcpy(&m.ipv4, rp->ai_addr, sizeof(struct sockaddr_in));
 			set_port(port);
-			memcpy(&m.ipv4.buffer[2], &addr->sin_addr.s_addr, 4);
-
 			freeaddrinfo(res);
 			return;
 		}
@@ -163,12 +94,8 @@ void ip_address::resolve(const char* host, uint16_t port, int v6) {
 			if(rp->ai_family != AF_INET6 || rp->ai_addrlen < sizeof(struct sockaddr_in6)) {
 				continue;
 			}
-			struct sockaddr_in6* addr = (struct sockaddr_in6*)rp->ai_addr;
-
-			m_type = IPV6;
+			memcpy(&m.ipv6, rp->ai_addr, sizeof(struct sockaddr_in6));
 			set_port(port);
-			memcpy(&m.ipv6.buffer[2], addr->sin6_addr.s6_addr, 16);
-
 			freeaddrinfo(res);
 			return;
 		}
@@ -179,83 +106,56 @@ void ip_address::resolve(const char* host, uint16_t port, int v6) {
 }
 
 ip_address::ip_address(const std::string& host, uint16_t port) :
-	address(IPV4)
+	address(AF_INET)
 {
 	resolve(host.c_str(), port, 1);
 }
 
 ipv4_address::ipv4_address(const std::string& host, uint16_t port) :
-	ip_address(IPV4)
+	ip_address(AF_INET)
 {
 	resolve(host.c_str(), port, 0);
 }
 
 ipv6_address::ipv6_address(const std::string& host, uint16_t port) :
-	ip_address(IPV6)
+	ip_address(AF_INET6)
 {
 	resolve(host.c_str(), port, 2);
 }
 
 
-void address::get_addr(sockaddr* addrbuf) const
+path_address::path_address(const std::string& path) :
+	address(AF_LOCAL)
 {
-	switch(m_type) {
-	case IPV4: {
-			sockaddr_in* addr = (sockaddr_in*)addrbuf;
-			memset(addr, 0, sizeof(sockaddr_in));
-			addr->sin_family = AF_INET;
-			addr->sin_port = htons(*(uint16_t*)m.ipv4.buffer);
-			memcpy(&addr->sin_addr.s_addr, m.ipv4.buffer+2, 4);
-			break;
-		}
-
-	case IPV6: {
-			sockaddr_in6* addr = (sockaddr_in6*)addrbuf;
-			memset(addr, 0, sizeof(sockaddr_in6));
-			addr->sin6_family = AF_INET6;
-			addr->sin6_port = htons(*(uint16_t*)m.ipv6.buffer);
-			memcpy(addr->sin6_addr.s6_addr, m.ipv6.buffer+2, 16);
-			break;
-		}
-
-	case PATH: {
-			sockaddr_un* addr = (sockaddr_un*)addrbuf;
-			memset(addr, 0, sizeof(sockaddr_un));
-			addr->sun_family = AF_LOCAL;
-			// FIXME check path length
-			strcpy(addr->sun_path, m.path.str);
-			break;
-		}
-
-	default:
-		// FIXME
-		break;
+	// FIXME check path length
+	struct sockaddr_un* addr = (struct sockaddr_un*)malloc(sizeof(struct sockaddr_un));
+	if(addr == NULL) {
+		throw std::bad_alloc();
 	}
+	memset(addr, 0, sizeof(struct sockaddr_un));
+	addr->sun_family = AF_LOCAL;
+	memcpy(addr->sun_path, path.c_str(), path.size()+1);
+	m.ex.addrlen = sizeof(struct sockaddr_un);
+	m.ex.addr = (struct sockaddr*)addr;
 }
 
 
 std::ostream& operator<< (std::ostream& stream, const address& a)
 {
-	switch(a.m_type) {
-	case address::IPV4: {
-			uint32_t sa = *(uint32_t*)(a.m.ipv4.buffer+2);
-			char buf[16];
-			return stream << inet_ntop(AF_INET, &sa, buf, sizeof(buf)) << ':' << ntohs(*(uint16_t*)a.m.ipv4.buffer);
-		}
+	if(a.family() == AF_INET) {
+		char buf[16];
+		return stream << inet_ntop(AF_INET, &a.m.ipv4.sin_addr, buf, sizeof(buf)) << ':' << ntohs(a.m.ipv4.sin_port);
 
-	case address::IPV6: {
-			unsigned char sa[16];
-			char buf[41];
-			memcpy(sa, a.m.ipv6.buffer+2, sizeof(sa));
-			return stream << '[' << ::inet_ntop(AF_INET6, sa, buf, sizeof(buf)) << "]:" << ntohs(*(uint16_t*)a.m.ipv6.buffer);
-		}
+	} else if(a.family() == AF_INET6) {
+		char buf[41];
+		return stream << '[' << ::inet_ntop(AF_INET6, &a.m.ipv6.sin6_addr, buf, sizeof(buf)) << "]:" << ntohs(a.m.ipv6.sin6_port);
 
-	case address::PATH:
-		return stream << a.m.path.str;
+	} else if(a.family() == AF_LOCAL) {
+		return stream << ((struct sockaddr_un*)a.m.ex.addr)->sun_path;
 
-	default:
+	} else {
 		// FIXME
-		return stream << "<unknown address>" << std::endl;
+		return stream << "<unknown address>";
 	}
 }
 
--- cpp/src/msgpack/rpc/address.h
+++ cpp/src/msgpack/rpc/address.h
@@ -40,19 +40,11 @@ public:
 	~address();
 
 public:
+	sa_family_t family() const { return m.ipv4.sin_family; }
+
 	socklen_t get_addrlen() const;
 	void get_addr(sockaddr* addrbuf) const;
 
-	typedef enum type_t {
-		IPV4 = 0,
-		IPV6 = 1,
-		PATH = 2,
-		UNKNOWN = 255,
-	} type_t;
-
-	type_t type() const { return m_type; }
-
-public:
 	bool operator== (const address& o) const;
 	bool operator!= (const address& o) const;
 	bool operator<  (const address& o) const;
@@ -65,43 +57,30 @@ public:
 	friend std::ostream& operator<< (std::ostream& stream, const address& a);
 
 protected:
-	type_t m_type;
-
 	union {
+		struct sockaddr_in  ipv4;
+		struct sockaddr_in6 ipv6;
 		struct {
-			// +--+----+
-			// | 2|  4 |
-			// +--+----+
-			// port network byte order
-			//    IPv4 address
-			char buffer[6];
-		} ipv4;
-
-		struct {
-			// +--+----------------+
-			// | 2|       16       |
-			// +--+----------------+
-			// port network byte order
-			//    IPv6 address
-			char buffer[18];
-		} ipv6;
-
-		struct {
-			char* str;
-		} path;
+			uint16_t family;  // don't reference. use family()
+			socklen_t addrlen;
+			struct sockaddr* addr;
+		} ex;
 	} m;
 
+	bool is_ex() const {
+		return m.ipv4.sin_family != AF_INET && m.ipv4.sin_family != AF_INET6 && m.ipv4.sin_family != 0;
+	}
+
+	void set_family(sa_family_t f) {
+		m.ipv4.sin_family = f;
+	}
+
 protected:
-	address(type_t t) : m_type(t) { }
+	address(sa_family_t f) { m.ipv4.sin_family = f; }
 
-public:
-	//// FIXME
-	//template <typename Packer>
-	//void msgpack_pack(Packer& pk) const;
-
-	//void msgpack_unpack(msgpack::object o);
-
-	//void msgpack_object(msgpack::object* o, msgpack::zone* z);
+private:
+	void copy_ex(const address& a);
+	void assign_ex(const address& a);
 };
 
 std::ostream& operator<< (std::ostream& stream, const address& a);
@@ -117,10 +96,11 @@ public:
 	void set_port(uint16_t port);
 
 protected:
-	ip_address(type_t t) : address(t) { }
+	ip_address(sa_family_t f) : address(f) { }
 
 	void resolve(const char* host, uint16_t port, int v6);
 };
+
 
 class ipv4_address : public ip_address {
 public:
@@ -128,11 +108,19 @@ public:
 	ipv4_address(const struct sockaddr_in& a);
 };
 
+
 class ipv6_address : public ip_address {
 public:
 	ipv6_address(const std::string& host, uint16_t port);
 	ipv6_address(const struct sockaddr_in6& a);
+
+	uint32_t get_flowinfo() const { return m.ipv6.sin6_flowinfo; }
+	void set_flowinfo(uint32_t v) { m.ipv6.sin6_flowinfo = v; }
+
+	uint32_t get_scope_id() const { return m.ipv6.sin6_scope_id; }
+	void set_scope_id(uint32_t v) { m.ipv6.sin6_scope_id = v; }
 };
+
 
 class path_address : public address {
 public:
@@ -144,94 +132,73 @@ public:
 
 inline address::address()
 {
-	m_type = UNKNOWN;
 	memset(&m, 0, sizeof(m));
 }
 
 inline address::~address()
 {
-	switch(m_type) {
-	case IPV4:
-	case IPV6:
-		break;
-
-	case PATH:
-		free(m.path.str);
-		break;
-
-	default:
-		break;
+	if(is_ex()) {
+		free(m.ex.addr);
 	}
 }
 
 inline address::address(const address& a)
 {
-	m_type = a.m_type;
-
-	memcpy(&m, &a.m, sizeof(m));
-
-	if(m_type == PATH) {
-		m.path.str = strdup(a.m.path.str);
-		if(m.path.str == NULL) {
-			throw std::bad_alloc();
-		}
+	if(a.is_ex()) {
+		copy_ex(a);
+	} else {
+		memcpy(&m, &a.m, sizeof(m));
 	}
 }
 
 inline const address& address::operator=(const address& o)
 {
-	if(m_type == PATH && o.m_type == PATH) {
-		size_t msize = strlen(m.path.str);
-		size_t asize = strlen(o.m.path.str);
-		if(msize >= asize) {
-			memcpy(m.path.str, o.m.path.str, asize+1);
-			return *this;
-		}
+	if(o.is_ex()) {
+		assign_ex(o);
+		return *this;
 	}
 
-	if(o.m_type == PATH) {
-		char* tmp = strdup(o.m.path.str);
-		if(tmp == NULL) {
-			throw std::bad_alloc();
-		}
-		if(m_type == PATH) {
-			free(m.path.str);
-		}
-		m.path.str = tmp;
-	} else {
-		memcpy(&m, &o.m, sizeof(m));
+	if(is_ex()) {
+		free(m.ex.addr);
 	}
+	memcpy(&m, &o.m, sizeof(m));
 
-	m_type = o.m_type;
 	return *this;
 }
 
 inline socklen_t address::get_addrlen() const
 {
-	switch(m_type) {
-	case IPV4:
-		return sizeof(sockaddr_in);
-	case IPV6:
-		return sizeof(sockaddr_in6);
-	case PATH:
-		return sizeof(sockaddr_un);
-	default:
-		return 0;
+	if(family() == AF_INET) {
+		return sizeof(struct sockaddr_in);
+	} else if(family() == AF_INET6) {
+		return sizeof(struct sockaddr_in6);
+	} else {
+		return m.ex.addrlen;
 	}
 }
 
-inline ip_address::ip_address(const struct sockaddr_in& a) :
-	address(IPV4)
+inline void address::get_addr(sockaddr* addrbuf) const
 {
-	memcpy(&m.ipv4.buffer[0], &a.sin_port, 2);
-	memcpy(&m.ipv4.buffer[2], &a.sin_addr.s_addr, 4);
+	if(family() == AF_INET) {
+		memcpy(addrbuf, &m.ipv4, sizeof(struct sockaddr_in));
+	} else if(family() == AF_INET6) {
+		memcpy(addrbuf, &m.ipv6, sizeof(struct sockaddr_in6));
+	} else {
+		memcpy(addrbuf, m.ex.addr, m.ex.addrlen);
+	}
 }
 
-inline ip_address::ip_address(const struct sockaddr_in6& a) :
-	address(IPV6)
+
+inline ip_address::ip_address(const struct sockaddr_in& a)
 {
-	memcpy(&m.ipv6.buffer[0], &a.sin6_port, 2);
-	memcpy(&m.ipv6.buffer[2], a.sin6_addr.s6_addr, 16);
+	m.ipv4 = a;
+	m.ipv4.sin_family = AF_INET;
+}
+
+inline ip_address::ip_address(const struct sockaddr_in6& a)
+{
+	m.ipv6 = a;
+	m.ipv6.sin6_family = AF_INET6;
 }
 
 inline ipv4_address::ipv4_address(const struct sockaddr_in& a) :
@@ -242,38 +209,33 @@ inline ipv6_address::ipv6_address(const struct sockaddr_in6& a) :
 
 inline uint16_t ip_address::get_port() const
 {
-	return ntohs(*(uint16_t*)m.ipv4.buffer);
+	return ntohs(m.ipv4.sin_port);
 }
 
 inline void ip_address::set_port(uint16_t port)
 {
-	*(uint16_t*)m.ipv4.buffer = htons(port);
+	m.ipv4.sin_port = htons(port);
 }
 
-inline path_address::path_address(const std::string& path) :
-	address(PATH)
-{
-	// FIXME check path length
-	m.path.str = strdup(path.c_str());
-}
 
 inline const char* path_address::get_path() const
 {
-	return m.path.str;
+	return ((struct sockaddr_un*)m.ex.addr)->sun_path;
 }
 
 
 inline bool address::operator== (const address& o) const
 {
-	switch(m_type) {
-	case IPV4:
-		return memcmp(m.ipv4.buffer, o.m.ipv4.buffer, sizeof(m.ipv4.buffer)) == 0;
-	case IPV6:
-		return memcmp(m.ipv6.buffer, o.m.ipv6.buffer, sizeof(m.ipv6.buffer)) == 0;
-	case PATH:
-		return strcmp(m.path.str, o.m.path.str) == 0;
-	default:
+	if(family() != o.family()) {
 		return false;
+	}
+	if(family() == AF_INET) {
+		return memcmp(&m.ipv4, &o.m.ipv4, sizeof(m.ipv4)) == 0;
+	} else if(family() == AF_INET6) {
+		return memcmp(&m.ipv6, &o.m.ipv6, sizeof(m.ipv6)) == 0;
+	} else {
+		return m.ex.addrlen == o.m.ex.addrlen &&
+			memcmp(m.ex.addr, o.m.ex.addr, m.ex.addrlen) == 0;
 	}
 }
 
@@ -284,127 +246,48 @@ inline bool address::operator!= (const address& o) const
 
 inline bool address::operator<  (const address& o) const
 {
-	switch(m_type) {
-	case IPV4:
-		return memcmp(m.ipv4.buffer, o.m.ipv4.buffer, sizeof(m.ipv4.buffer)) < 0;
-	case IPV6:
-		return memcmp(m.ipv6.buffer, o.m.ipv6.buffer, sizeof(m.ipv6.buffer)) < 0;
-	case PATH:
-		return strcmp(m.path.str, o.m.path.str) < 0;
-	default:
-		return 0;
+	if(family() != o.family()) {
+		// FIXME platform dependent
+		return family() < o.family();
 	}
+	if(family() == AF_INET) {
+		return memcmp(&m.ipv4, &o.m.ipv4, sizeof(struct sockaddr_in)) < 0;
+	} else if(family() == AF_INET6) {
+		return memcmp(&m.ipv6, &o.m.ipv6, sizeof(struct sockaddr_in6)) < 0;
+	}
+	if(m.ex.addrlen != o.m.ex.addrlen) {
+		return m.ex.addrlen < o.m.ex.addrlen;
+	}
+	return memcmp(&m.ex.addr, &o.m.ex.addr, m.ex.addrlen) < 0;
 }
 
 inline bool address::operator>  (const address& o) const
 {
-	switch(m_type) {
-	case IPV4:
-		return memcmp(m.ipv4.buffer, o.m.ipv4.buffer, sizeof(m.ipv4.buffer)) > 0;
-	case IPV6:
-		return memcmp(m.ipv6.buffer, o.m.ipv6.buffer, sizeof(m.ipv6.buffer)) > 0;
-	case PATH:
-		return strcmp(m.path.str, o.m.path.str) > 0;
-	default:
-		return 0;
+	if(family() != o.family()) {
+		// FIXME platform dependent
+		return family() > o.family();
 	}
+	if(family() == AF_INET) {
+		return memcmp(&m.ipv4, &o.m.ipv4, sizeof(struct sockaddr_in)) > 0;
+	} else if(family() == AF_INET6) {
+		return memcmp(&m.ipv6, &o.m.ipv6, sizeof(struct sockaddr_in6)) > 0;
+	}
+	if(m.ex.addrlen != o.m.ex.addrlen) {
+		return m.ex.addrlen > o.m.ex.addrlen;
+	}
+	return memcmp(&m.ex.addr, &o.m.ex.addr, m.ex.addrlen) > 0;
 }
 
 inline size_t address::hash::operator() (const msgpack::rpc::address& a) const
 {
-	size_t h = mp::hash<int>()(a.m_type);
-	switch(a.m_type) {
-	case IPV4:
-		return h ^ mp::hash<std::string>()(std::string(a.m.ipv4.buffer, sizeof(a.m.ipv4.buffer)));
-	case IPV6:
-		return h ^ mp::hash<std::string>()(std::string(a.m.ipv6.buffer, sizeof(a.m.ipv6.buffer)));
-	case PATH:
-		return h ^ mp::hash<std::string>()(std::string(a.m.path.str));
-	default:
-		return h;
+	if(a.family() == AF_INET) {
+		return mp::hash<std::string>()(std::string((const char*)&a.m.ipv4, sizeof(struct sockaddr_in)));
+	} else if(a.family() == AF_INET6) {
+		return mp::hash<std::string>()(std::string((const char*)&a.m.ipv6, sizeof(struct sockaddr_in6)));
+	} else {
+		return mp::hash<std::string>()(std::string((const char*)&a.m.ex.addr, a.m.ex.addrlen));
 	}
 }
-
-// FIXME
-//
-//template <typename Packer>
-//void address::msgpack_pack(Packer& pk) const
-//{
-//	pk.pack_array(2);
-//
-//	pk.pack_int(m_type);
-//
-//	switch(m_type) {
-//	case IPV4:
-//		pk.pack_raw(sizeof(m.ipv4.buffer));
-//		pk.pack_raw_body(m.ipv4.buffer, sizeof(m.ipv4.buffer));
-//		break;
-//
-//	case IPV6:
-//		pk.pack_raw(sizeof(m.ipv6.buffer));
-//		pk.pack_raw_body(m.ipv6.buffer, sizeof(m.ipv6.buffer));
-//		break;
-//
-//	case PATH: {
-//			size_t size = strlen(m.path.str);
-//			pk.pack_raw(size);
-//			pk.pack_raw_body(m.path.str, size);
-//			break;
-//		}
-//
-//	default:
-//		// FIXME
-//		throw std::runtime_error("unknown address type");
-//	}
-//}
-//
-//void address::msgpack_unpack(msgpack::object o)
-//{
-//
-//	if(o.type != type::ARRAY) { throw type_error(); }
-//	if(o.via.array.size < 2) { throw type_error(); }
-//
-//	int type;
-//	o.via.array.ptr[0].convert(&type);
-//
-//	msgpack::object p = o.via.array.ptr[1];
-//
-//	// FIXME free(m.path.str);
-//
-//	switch(type) {
-//	case IPV4:
-//		if(p.type != type::RAW) { throw type_error(); }
-//		if(p.via.raw.size != sizeof(m.ipv4.buffer)) { throw type_error(); }
-//		memcpy(m.ipv4.buffer, p.via.raw.ptr, sizeof(m.ipv4.buffer));
-//		break;
-//
-//	case IPV6:
-//		if(p.type != type::RAW) { throw type_error(); }
-//		if(p.via.raw.size != sizeof(m.ipv6.buffer)) { throw type_error(); }
-//		memcpy(m.ipv6.buffer, p.via.raw.ptr, sizeof(m.ipv6.buffer));
-//		break;
-//
-//	case PATH:
-//		if(p.type != type::RAW) { throw type_error(); }
-//		{
-//			char* tmp = malloc(p.via.raw.size+1);
-//			if(tmp == NULL) {
-//				throw std::bad_alloc();
-//			}
-//			memcpy(tmp, p.via.raw.ptr, p.via.raw.size);
-//			tmp[p.via.raw.size] = '\0';
-//			m.path.str = tmp;
-//		}
-//		break;
-//
-//	default:
-//		throw msgpack::type_error("unknown address type");
-//	}
-//}
-//
-//void address::msgpack_object(msgpack::object* o, msgpack::zone* z)
-//{
-//}
 
 
 }  // namespace rpc
