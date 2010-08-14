0a766fc17d3c3af17015887f78ab68ba5b5c4051
frsyuki <frsyuki@users.sourceforge.jp>
2010-08-14 18:36:49 +0900

    cpp: adds attack_callback test

---
+++ cpp/test/attack_callback.cc
@@ -0,0 +1,74 @@
+#include "attack.h"
+#include <cclog/cclog.h>
+#include <cclog/cclog_tty.h>
+#include <vector>
+#include <mp/functional.h>
+
+static size_t ATTACK_DEPTH;
+static size_t ATTACK_THREAD;
+static size_t ATTACK_LOOP;
+static size_t ATTACK_SIZE = 1024;
+
+static std::auto_ptr<attacker> test;
+static std::auto_ptr<rpc::session_pool> sp;
+
+using msgpack::type::raw_ref;
+using namespace mp::placeholders;
+
+void callback_func(rpc::future f, raw_ref msg, rpc::shared_zone msglife)
+{
+	raw_ref result = f.get<raw_ref>();
+
+	if(result.size != msg.size) {
+		LOG_ERROR("invalid size: ",result.size);
+	} else if(memcmp(result.ptr, msg.ptr, msg.size) != 0) {
+		LOG_ERROR("received data don't match with sent data.");
+	}
+}
+
+void attack_callback()
+{
+	std::vector<rpc::future> pipeline(ATTACK_DEPTH);
+
+	for(size_t i=0; i < ATTACK_LOOP; ++i) {
+		rpc::session s = sp->get_session(test->address());
+		s.set_timeout(30.0);
+
+		rpc::shared_zone msglife(new msgpack::zone());
+		raw_ref msg = raw_ref((char*)msglife->malloc(ATTACK_SIZE), ATTACK_SIZE);
+
+		for(size_t j=0; j < ATTACK_DEPTH; ++j) {
+			pipeline[j] = s.call("echo_huge", msglife, msg).attach_callback(
+					mp::bind(callback_func, _1, msg, msglife));
+		}
+
+		for(size_t j=0; j < ATTACK_DEPTH; ++j) {
+			pipeline[j].join();
+		}
+	}
+}
+
+int main(void)
+{
+	cclog::reset(new cclog_tty(cclog::WARN, std::cout));
+
+	ATTACK_DEPTH  = attacker::option("DEPTH",  25, 100);
+	ATTACK_THREAD = attacker::option("THREAD", 25, 100);
+	ATTACK_LOOP   = attacker::option("LOOP",   5, 50);
+
+	std::cout << "callback attack"
+		<< " depth="  << ATTACK_DEPTH
+		<< " thread=" << ATTACK_THREAD
+		<< " loop="   << ATTACK_LOOP
+		<< std::endl;
+
+	test.reset(new attacker());
+
+	sp.reset(new rpc::session_pool(test->builder()));
+	sp->start(4);
+
+	test->run(ATTACK_THREAD, &attack_callback);
+
+	return 0;
+}
+
--- cpp/test/attack_huge.cc
+++ cpp/test/attack_huge.cc
@@ -6,23 +6,24 @@ static size_t ATTACK_SIZE;
 static size_t ATTACK_THREAD;
 static size_t ATTACK_LOOP;
 
-static char* data;
-
 static std::auto_ptr<attacker> test;
+
+using msgpack::type::raw_ref;
+
+static rpc::shared_zone msglife;
+static raw_ref msg;
 
 void attack_huge()
 {
 	rpc::client c(test->builder(), test->address());
-	c.set_timeout(30.0);
-
-	using msgpack::type::raw_ref;
+	c.set_timeout(60.0);
 
 	for(size_t i=0; i < ATTACK_LOOP; ++i) {
-		raw_ref result = c.call("echo_huge", raw_ref(data, ATTACK_SIZE)).get<raw_ref>();
+		raw_ref result = c.call("echo_huge", msglife, msg).get<raw_ref>();
 
-		if(result.size != ATTACK_SIZE) {
+		if(result.size != msg.size) {
 			LOG_ERROR("invalid size: ",result.size);
-		} else if(memcmp(result.ptr, data, ATTACK_SIZE) != 0) {
+		} else if(memcmp(result.ptr, msg.ptr, msg.size) != 0) {
 			LOG_ERROR("received data don't match with sent data.");
 		}
 	}
@@ -32,7 +33,7 @@ int main(void)
 {
 	cclog::reset(new cclog_tty(cclog::WARN, std::cout));
 
-	ATTACK_SIZE   = attacker::option("SIZE",   1024*1024, 8*1024*1024);
+	ATTACK_SIZE   = attacker::option("SIZE",   1024*1024, 4*1024*1024);
 	ATTACK_THREAD = attacker::option("THREAD", 5, 20);
 	ATTACK_LOOP   = attacker::option("LOOP",   2, 20);
 
@@ -42,11 +43,8 @@ int main(void)
 		<< " loop="   << ATTACK_LOOP
 		<< std::endl;
 
-	data = (char*)malloc(ATTACK_SIZE);
-	if(data == NULL) {
-		LOG_FATAL("failed to malloc data");
-		return 1;
-	}
+	msglife.reset(new msgpack::zone());
+	msg = raw_ref((char*)msglife->malloc(ATTACK_SIZE), ATTACK_SIZE);
 
 	test.reset(new attacker());
 	test->run(ATTACK_THREAD, &attack_huge);
