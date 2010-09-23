5e4b184716d44af057f25fda1aaf8b6e6b35ca29
Muga Nishizawa <muga@f11vm.(none)>
2010-09-23 21:01:24 +0900

    java: change a program for dynamic code generation

--- java/src/test/java/org/msgpack/rpc/util/codegen/ServerTest.java
+++ java/src/test/java/org/msgpack/rpc/util/codegen/ServerTest.java
@@ -8,8 +8,18 @@ import org.msgpack.rpc.Client;
 import org.msgpack.rpc.EventLoop;
 import org.msgpack.rpc.Request;
 import org.msgpack.rpc.Server;
+import org.msgpack.util.annotation.PackUnpackUtil;
 
 public class ServerTest extends TestCase {
+    public static class Foo {
+        public int i;
+
+        public int j;
+
+        public Foo() {
+        }
+    }
+
     public static class TestHandler {
         public int m0(int i, int j) {
             return i + j;
@@ -20,9 +30,13 @@ public class ServerTest extends TestCase {
             int ret = packObjs[0].intValue() + packObjs[1].intValue();
             req.sendResponse(ret, null);
         }
-        
+
         public void m2(Request req, int i, int j) {
             req.sendResponse(i + j, null);
+        }
+
+        public int m3(Foo foo) {
+            return foo.i + foo.j;
         }
     }
 
@@ -33,19 +47,30 @@ public class ServerTest extends TestCase {
         Client c = new Client("127.0.0.1", 19850);
 
         try {
+            PackUnpackUtil.registerEnhancedClass(Foo.class, true);
             svr.serve(new DynamicCodegenDispatcher(new TestHandler()));
             svr.listen(19850);
 
-            int num = 100;
+            int num = 1000;
 
             long start = System.currentTimeMillis();
             for (int i = 0; i < num; i++) {
-                MessagePackObject ret0 = c.callApply("m0", new Object[] { i, i + 1 });
+                MessagePackObject ret0 = c.callApply("m0", new Object[] { i,
+                        i + 1 });
                 assertEquals(2 * i + 1, ret0.intValue());
-                MessagePackObject ret1 = c.callApply("m1", new Object[] { i, i + 1 });
+                MessagePackObject ret1 = c.callApply("m1", new Object[] { i,
+                        i + 1 });
                 assertEquals(2 * i + 1, ret1.intValue());
-                MessagePackObject ret2 = c.callApply("m2", new Object[] { i, i + 1 });
+                MessagePackObject ret2 = c.callApply("m2", new Object[] { i,
+                        i + 1 });
                 assertEquals(2 * i + 1, ret2.intValue());
+                Foo foo = (Foo) PackUnpackUtil.newEnhancedInstance(Foo.class,
+                        true);
+                foo.i = i;
+                foo.j = i + 1;
+                MessagePackObject ret3 = c
+                        .callApply("m3", new Object[] { foo });
+                assertEquals(2 * i + 1, ret3.intValue());
             }
             long finish = System.currentTimeMillis();
 
@@ -58,7 +83,7 @@ public class ServerTest extends TestCase {
             loop.shutdown();
         }
     }
-    
+
     @Test
     public void testAsyncLoad() throws Exception {
         EventLoop loop = new EventLoop();
@@ -66,22 +91,28 @@ public class ServerTest extends TestCase {
         Client c = new Client("127.0.0.1", 19850);
 
         try {
+            PackUnpackUtil.registerEnhancedClass(Foo.class, true);
             svr.serve(new DynamicCodegenDispatcher(new TestHandler()));
             svr.listen(19850);
 
             int num = 100;
 
             long start = System.currentTimeMillis();
-            for(int i=0; i < num-1; i++) {
+            for (int i = 0; i < num - 1; i++) {
                 c.notifyApply("m0", new Object[] { i, i + 1 });
                 c.notifyApply("m1", new Object[] { i, i + 1 });
                 c.notifyApply("m2", new Object[] { i, i + 1 });
+                Foo foo = (Foo) PackUnpackUtil.newEnhancedInstance(Foo.class,
+                        true);
+                foo.i = i;
+                foo.j = i + 1;
+                c.notifyApply("m3", new Object[] { foo });
             }
             MessagePackObject ret0 = c.callApply("m0", new Object[] { 0, 1 });
             long finish = System.currentTimeMillis();
 
-            double result = num / ((double)(finish - start) / 1000);
-            System.out.println("async: "+result+" calls per sec");
+            double result = num / ((double) (finish - start) / 1000);
+            System.out.println("async: " + result + " calls per sec");
 
         } finally {
             svr.close();
