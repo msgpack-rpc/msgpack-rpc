37959c408a91e6c8583020de400cf15ecfbaeb2d
UENISHI Kota <kuenishi@gmail.com>
2011-04-03 18:18:07 +0900

    rebar'ized and eunit'ized

--- erlang/src/mp_client.erl
+++ erlang/src/mp_client.erl
@@ -45,7 +45,6 @@
 -module(mp_client).
 
 -behaviour(gen_server).
--include_lib("eunit/include/eunit.hrl").
 -include("mp_rpc.hrl").
 
 -define(SERVER, ?MODULE).
@@ -264,9 +263,38 @@ code_change(_OldVsn, State, _Extra) ->
 %%--------------------------------------------------------------------
 %%% Internal functions
 %%--------------------------------------------------------------------
--ifdef(EUNIT).
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
 
-%% my_test()->
+my_first_case(_Config) ->
+    {ok, _Pid}=mp_client:connect(localhost,65500),
+    {ok, Result}=mp_client:call(42, hello, []),
+    true=is_list(Result),
+    ok=mp_client:close().
+
+my_second_case(_)->
+    {ok, _}=mp_client:connect({local, hoge}, localhost,65500),
+    {ok, Result}=mp_client:call(hoge,42, hello, []),
+    true=is_list(Result),
+    {ok, 7}=mp_client:call(hoge,43, add, [3,4]),
+    ok=mp_client:close(hoge).
+
+case_add(Config)->
+    Pairs=[{5,5}, {0,0}, {234, 2}, {213456789, -3}, {234, -23}, {-1,1}, {1,-1}, {-1,-1},
+	  {-2000, 2000}, {2000, -2000}, {234, -234}],
+    {ok, _Pid}=mp_client:connect({local,add}, localhost,65500),
+    {ok, _Result}=mp_client:call(add, 42, hello, []),
+    lists:map( fun({L,R})-> S=L+R, {ok,S}=mp_client:call(add, (L+42), add, [L,R])  end, Pairs ),
+    {error, {<<"no such func">>,nil}}=mp_client:call(add, 890, no_such_func, []),
+    mp_client:close(add).
+
+my_test()->
+    ok=sample_app:start(),
+    ok=my_first_case(nil),
+    ok=my_second_case(nil),
+    ok=case_add(nil),
+    ok=sample_app:stop().
+    
 %%     {ok,Pid}=mp_client:connect(localhost,65500),
 %%     {ok,_Reply}=mp_client:call(Pid, hoge, []),
 %%     mp_client:close().
