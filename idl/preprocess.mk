5ae93dc8254079f067c968206d4aef8c79d5c377
Moriyoshi Koizumi <mozo@mozo.jp>
2011-04-03 18:41:18 +0900

    Rewrite preprocess.mk to be bundler-based.

--- idl/preprocess.mk
+++ idl/preprocess.mk
@@ -1,37 +1,22 @@
-RUBY=ruby
-TTVER=treetop-1.4.8
+BUNDLE=bundle
+VENDOR_BUNDLE_DIR=vendor/bundle
 
 all: parser.rb
 
-TTURL = http://rubygems.org/downloads/$(TTVER).gem
+$(VENDOR_BUNDLE_DIR): Gemfile
+	$(BUNDLE) install --path $(VENDOR_BUNDLE_DIR)
 
-$(TTVER).gem:
-	wget $(TTURL) || curl -O $(TTURL) || fetch $(TTURL)
+parser.tt: parser.tt.mpl mplex.rb $(VENDOR_BUNDLE_DIR)
+	$(BUNDLE) exec mplex -o $@ $<
 
-$(TTVER)/bin/tt: $(TTVER).gem
-	mkdir -p $(TTVER)
-	cd $(TTVER) && tar xvf ../$(TTVER).gem
-	cd $(TTVER) && tar zxvf data.tar.gz
-	touch $@
-
-rubylib/rubygems: $(TTVER)/bin/tt
-	mkdir -p rubylib
-	cp -rf $(TTVER)/lib/* rubylib/
-	cp -rf $(TTVER)/README.md rubylib/treetop/
-	cp -rf $(TTVER)/LICENSE rubylib/treetop/
-	echo "# dummy" > $@
-
-parser.tt: parser.tt.mpl mplex.rb
-	$(RUBY) -rmplex -e 'Mplex.write("parser.tt.mpl", "$@")'
-
-parser.rb: $(TTVER)/bin/tt parser.tt rubylib/rubygems
-	$(RUBY) -I$(TTVER)/lib $(TTVER)/bin/tt -o $@ parser.tt
+parser.rb: parser.tt $(VENDOR_BUNDLE_DIR)
+	$(BUNDLE) exec tt -o $@ parser.tt
 
 clean:
-	rm -rf $(TTVER) rubylib parser.tt parser.rb
+	rm -rf parser.tt parser.rb
 
 distclean: clean
-	rm -rf $(TTVER).gem
+	rm -rf $(VENDOR_BUNDLE_DIR)
 
 .PHONY: all clean distclean
 
